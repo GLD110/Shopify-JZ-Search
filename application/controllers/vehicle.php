09402787352ee6f3d35cfab8d3a730e4b53fa71a
diana4545 <diana4545@outlook.com>
2019-01-17 19:23:33 +0000

    update

--- application/controllers/vehicle.php
+++ application/controllers/vehicle.php
@@ -21,26 +21,26 @@ class Vehicle extends MY_Controller {
       $this->manageVehicle();
   }
 
-  // function csv_to_array($filename='', $delimiter=',')
-  // {
-  // 	if(!file_exists($filename) || !is_readable($filename))
-  // 		return FALSE;
-  //
-  // 	$header = NULL;
-  // 	$data = array();
-  // 	if (($handle = fopen($filename, 'r')) !== FALSE)
-  // 	{
-  // 		while (($row = fgetcsv($handle, 1000, $delimiter)) !== FALSE)
-  // 		{
-  // 			if(!$header)
-  // 				$header = $row;
-  // 			else
-  // 				$data[] = array_combine($header, $row);
-  // 		}
-  // 		fclose($handle);
-  // 	}
-  // 	return $data;
-  // }
+  function csv_to_array($filename='', $delimiter=',')
+  {
+  	if(!file_exists($filename) || !is_readable($filename))
+  		return FALSE;
+
+  	$header = NULL;
+  	$data = array();
+  	if (($handle = fopen($filename, 'r')) !== FALSE)
+  	{
+  		while (($row = fgetcsv($handle, 1000, $delimiter)) !== FALSE)
+  		{
+  			if(!$header)
+  				$header = $row;
+  			else
+  				$data[] = array_combine($header, $row);
+  		}
+  		fclose($handle);
+  	}
+  	return $data;
+  }
 
   function manageVehicle(){
       // Check the login
