4e96b86a56c8e0a6a591df8d68cf227599d1f43d
diana4545 <diana4545@outlook.com>
2019-03-20 13:41:59 +0000

    updatemake and delmodel

--- application/controllers/product.php
+++ application/controllers/product.php
@@ -349,31 +349,31 @@ class Product extends MY_Controller {
      }
   }
 
-  function updateMake( $key ){
-    if($this->session->userdata('role') == 'admin'){
-      $val = $this->input->post('value');
-      $pk =  $this->input->post('pk');
-      $data = array(
-        $key => $val
-      );
-
-      $this->Make_model->update( $pk, $data );
-    }
-  }
-
-  function manageModel(){
-      // Check the login
-      $this->is_logged_in();
-
-      if($this->session->userdata('role') == 'admin'){
-          $data['query'] =  $this->Model_model->getList();
-          $data['arrStoreList'] =  $this->_arrStoreList;
-
-          $this->load->view('view_header');
-          $this->load->view('view_model', $data);
-          $this->load->view('view_footer');
-      }
-  }
+  // function updateMake( $key ){
+  //   if($this->session->userdata('role') == 'admin'){
+  //     $val = $this->input->post('value');
+  //     $pk =  $this->input->post('pk');
+  //     $data = array(
+  //       $key => $val
+  //     );
+  //
+  //     $this->Make_model->update( $pk, $data );
+  //   }
+  // }
+  //
+  // function manageModel(){
+  //     // Check the login
+  //     $this->is_logged_in();
+  //
+  //     if($this->session->userdata('role') == 'admin'){
+  //         $data['query'] =  $this->Model_model->getList();
+  //         $data['arrStoreList'] =  $this->_arrStoreList;
+  //
+  //         $this->load->view('view_header');
+  //         $this->load->view('view_model', $data);
+  //         $this->load->view('view_footer');
+  //     }
+  // }
 
   function delModel(){
       if($this->session->userdata('role') == 'admin'){
